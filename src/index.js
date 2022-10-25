08d618662ae5a4879b18075e5fd6f5435568fb7b
daxthin <nicolasdga.dev@gmail.com>
2022-10-25 02:27:10 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 function Parallax() {
-    let zoom = 300;
+    let scroll = 300;
     // let saturate = 100;
     document.addEventListener('scroll', (e) => {
         document.querySelectorAll('.parallax-item').forEach((item) => {
+
+            // store element parallax speed
             const speed = item.getAttribute('data-speed');
+
+            // get scroll value
             let top = document.documentElement.scrollTop;
 
-            zoom = top * speed;
-            zoom = Math.min(Math.max(zoom, 0), 300);
+            // control element speed
+            scroll = top * speed;
+
+            // clamp scroll value
+            scroll = Math.min(Math.max(scroll, 0), 300);
 
             // saturate = top;
             // saturate = Math.min(Math.max(saturate, 0), 400) / 100;
@@ -15,7 +22,7 @@ function Parallax() {
             
             // console.log(Remap(saturate, 0, 4, 4, 0));
 
-            item.style.transform = `translateY(${-zoom}px)`;
+            item.style.transform = `translateY(${-scroll}px)`;
             // document.getElementById('bg').style = `filter: saturate(${Remap(saturate, 0, 4, 1, 0)}); transition: 50ms ease; transform: translateY(${-zoom}px)`;
         });
     });
@@ -24,30 +31,42 @@ function Parallax() {
 let fadeWelcome = document.querySelectorAll('.welcome')[0];
 let content = document.querySelectorAll('.content')[0];
 
+// welcome anim functions
 document.addEventListener('DOMContentLoaded', () => {
 
+    // set start delay for fade in
     setTimeout(() => {
         fadeWelcome.classList.add('fadeIn');
         document.body.style = 'overflow: hidden;';
-        document.getElementById('bg').style = "filter: saturate(0); transition: 250ms ease-in-out"
-    }, 50);
+        document.getElementById('bg').style = `filter: saturate(0); 
+                                               transition: 250ms ease-in-out`;
+    },
+    50);
     
+    // set before delay for fade out
     setTimeout(() => {
         fadeWelcome.classList.add('fadeOut');
         fadeWelcome.classList.remove('fadeIn');
         fadeWelcome.style = 'visibility: hidden;';
-        document.getElementById('bg').style = "filter: saturate(1); transition: 250ms ease-in-out"
-        content.style = 'transform: translateY(0%); transition: 1000ms transform ease-in-out;';
+        document.getElementById('bg').style = `filter: saturate(1); 
+                                               transition: 250ms ease-in-out`;
+        content.style = `transform: translateY(0%); 
+                         transition: 1000ms
+                         transform ease-in-out;`;
         enablePageInteraction();
-    }, 2500);
+    },
+    2500);
 
 }, false);
 
-
 function enablePageInteraction() {
     setTimeout(() => {
-        content.style = 'transform: translateY(0%); transition: 50ms transform ease;';
-        document.getElementById('bg').style = "filter: saturate(1); transition: 50ms ease"
+        content.style = `transform: translateY(0%); 
+                         transition: 50ms transform ease;`;
+        
+        document.getElementById('bg').style = `filter: saturate(1); 
+                                               transition: 50ms ease`;
+        
         document.body.style = 'overflow: scroll;';
         document.querySelectorAll('p').forEach((p) => {
             p.addEventListener('click', () => {
@@ -56,7 +75,8 @@ function enablePageInteraction() {
             })
         })
         Parallax();
-    }, 1000);
+    },
+    1000);
 }
 
 function Remap(value, low1, high1, low2, high2) {
