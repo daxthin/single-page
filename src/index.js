5e8eaebd98dac49e469a926b85395a13240ad207
daxthin <nicolasdga.dev@gmail.com>
2022-10-27 06:52:17 -0300

    ui changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-let fadeWelcome = document.querySelectorAll('.welcome')[0];
-let content = document.querySelectorAll('.content')[0];
+
+const fadeWelcome = document.querySelectorAll('.welcome')[0];
+const content = document.querySelectorAll('.content')[0];
+const toggleThemeBtn = document.getElementById('toggle-theme');
+let darkMode;
+const limitForIconFade = 300;
+const images = document.querySelectorAll('img');
+
+setTimeout(() => {
+    window.scrollTo(0, 0);
+}, 100);
+
 
 // !TOGGLE DARK MODE >
-let toggleThemeBtn = document.getElementById('toggle-theme');
-
-let darkMode;
-let limitForIconFade = 200;
-
 if (darkMode != null) {
     window.localStorage.setItem('dark', false);
 }
@@ -33,156 +38,45 @@ function ToggleDarkMode(mode) {
     }
 }
 
-document.addEventListener('scroll', (e) => {
-        // get scroll value
-        let top = document.documentElement.scrollTop;
-    
-        toggleThemeBtn.style.opacity = `${Remap(top, 0, limitForIconFade, 1, 0)}`;
-});
+function fadeDarkModeBtn() {
+    let top = document.documentElement.scrollTop;
+    toggleThemeBtn.style.opacity = `${Remap(top, 0, limitForIconFade, 1, 0)}`;
+}
 
+// !SECTION HIGHLIGHT >
+// *FIXED DETECTION RANGE
+const elementsForHighLight = () => {
+    let list = [];
+    list.push();
+    list.push(document.getElementsByTagName('article'));
+    return list;
+}
 
-
-
-
-// *TOGGLE DARK MODE <
-
-
-
-// !FADE IN OUT WELCOME TEXT >
-function FadeInOut(aT, bT) {
-    let delayIn = aT;
-    let delayOut = bT;
-
-  
-    document.querySelectorAll('.fade-element').forEach((e) => {
-
-        delayIn *= 2;
-        delayOut *= 2;
-
-        setTimeout(() => {
-            e.classList.add('elementFadeIn');
-        }, delayIn);
-        setTimeout(() => {
-            e.classList.add('elementFadeOut');
-        }, delayOut);
-
-
+function highlightElements() {
+    images.forEach(e => {
+        let viewportHeight = window.innerHeight;
+        let { top, bottom } = e.getBoundingClientRect();
+        // get the center of the element
+        let center = top + bottom / 2;
+        //set a range between 20 and 100 % of the viewport 
+        if( center > viewportHeight*0.2 && center < viewportHeight){
+            e.style.filter = 'saturate(1)';
+            e.style.maxWidth = '500px';
+            e.style.transition = '500ms ease-in-out';
+        }  
+        else {
+            e.style.filter = 'saturate(0)';
+            e.style.maxWidth = '400px';
+            e.style.transition = '500ms ease-in-out';
+        }   
     })
-
-    return delayOut;
 }
-// *FADE IN OUT WELCOME TEXT <
-
-// 
-
-// !CONTENT PARALLAX >
-function Parallax() {
-    let scroll = 300;
-    document.addEventListener('scroll', (e) => {
-        document.querySelectorAll('.parallax-item').forEach((item) => {
-            // store element parallax speed
-            const speed = item.getAttribute('data-speed');
-
-            // get scroll value
-            let top = document.documentElement.scrollTop;
-            
-            // control element speed
-            scroll = top * speed;
-
-            // clamp scroll value
-            scroll = Math.min(Math.max(scroll, 0), 300);
-            item.style.transform = `translateY(${-scroll}px)`;
-        });
-    });
-}
-// *CONTENT PARALLAX <
-
-
 
 
 document.addEventListener('scroll', (e) => {
-    document.querySelectorAll('.parallax-item').forEach((item) => {
-        document.querySelectorAll('img').forEach((e) => {
-            let top = document.documentElement.scrollTop;
-            let viewportHeight = window.innerHeight;
-            let y = e.getBoundingClientRect().top;
-
-   
-
-            if( y > viewportHeight/5.5 && y < viewportHeight/1.5 ){
-                e.style.filter = 'saturate(1)';
-                e.style.transition = '500ms ease-in-out';
-    
-            }  
-            else {
-                e.style.filter = 'saturate(0)';
-                e.style.transition = '500ms ease-in-out';
-
-            }
-
-        })
-    })
+    requestAnimationFrame(fadeDarkModeBtn);
+    requestAnimationFrame(highlightElements);
 });
-    
-    
-    
-// !WELCOME ANIM FUNCTION >
-document.addEventListener('DOMContentLoaded', () => {
-
-    // !set start delay for fade in to avoid load first
-    setTimeout(() => {
-        fadeWelcome.classList.add('welcomeFadeIn');
-        document.body.style = 'overflow: hidden;';
-        document.getElementById('bg').style = `filter: saturate(0); transition: 250ms ease-in-out`;
-    },
-    50);
-    
-    // *set delay for fade out after 2500
-    setTimeout(() => {
-        fadeWelcome.classList.add('welcomeFadeOut');
-        fadeWelcome.classList.remove('welcomeFadeIn');
-        document.getElementById('bg').style = `filter: saturate(1); transition: 250ms ease-in-out`;
-        
-        content.style = `transform: translateY(0%); 
-                         transition: 1000ms
-                         transform ease-in-out;`;
-        enablePageInteraction();
-    },
-    FadeInOut(500, 800));
-
-}, false);
-// *WELCOME ANIM FUNCTION <
-
-
-
-
-// !ENABLE PAGE FUNCTIONS >
-/**
- * *Allow user to interact after welcome test
- */
-function enablePageInteraction() {
-    setTimeout(() => {
-        content.style = `transform: translateY(0%); transition: 400ms ease;`;
-        
-        document.getElementById('bg').style = `filter: saturate(1); transition: 50ms ease`;
-        
-        document.body.style = 'overflow: scroll;';
-        
-        document.querySelectorAll('p').forEach((p) => {
-            p.addEventListener('click', () => {
-                p.contentEditable = 'true';
-            })
-        })
-        fadeWelcome.style = 'display: none;';
-        Parallax();
-    },
-    1000);
-}
-// *ENABLE PAGE FUNCTIONS <
-
-
-
-
 
 // !OTHER FUNCTIONS FOR UTILITY >
 /**
@@ -204,15 +98,3 @@ function Remap(value, low1, high1, low2, high2) {
                   (value - low1) /
                   (high1 - low1);
 }
-
-function easeInOut(t) {
-    if (t < 0.5) {
-        return 2.0 * t * t;
-    }
-    else
-    {
-        t -= 0.5;
-        return 2.0 * t * (1.0 - t) + 0.5;
-    }
-}
-// *OTHER FUNCTIONS FOR UTILITY <
